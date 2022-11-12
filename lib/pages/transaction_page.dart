4d74883172ba6094336ef0110b97517b27775a40
humaidi <humaidizakaria1998@gmail.com>
2022-11-12 20:01:05 +0700

    Insert transaksi

--- lib/pages/transaction_page.dart
+++ lib/pages/transaction_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:uangkooh/pages/models/database.dart';
 
 class TransactionPage extends StatefulWidget {
   const TransactionPage({super.key});
@@ -10,11 +11,43 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
+  final AppDb database = AppDb();
   bool pengeluaran = true;
+  late int type;
   List<String> list = ['Makan', 'Transport', 'Judi Online'];
+  Category? selectedCategory;
 
   late String dropdownValue = list.first;
   TextEditingController dateController = TextEditingController();
+  TextEditingController jumlahController = TextEditingController();
+  TextEditingController deskripsiController = TextEditingController();
+
+  // insert transaksi
+  Future insert(
+      int jumlah, DateTime date, String nameDeskripsi, int categoryId) async {
+    DateTime now = DateTime.now();
+    await database.into(database.transactions).insertReturning(
+          TransactionsCompanion.insert(
+            name: nameDeskripsi,
+            category_id: categoryId,
+            transaction_date: date,
+            amount: jumlah,
+            createdAt: now,
+            updatedAt: now,
+          ),
+        );
+  }
+
+  // get allCategory where type
+  Future<List<Category>> getAllCategory(int type) async {
+    return await database.getAllCategoryRepo(type);
+  }
+
+  @override
+  void initState() {
+    type = 2;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +67,8 @@ class _TransactionPageState extends State<TransactionPage> {
                     onChanged: (bool value) {
                       setState(() {
                         pengeluaran = value;
+                        type = pengeluaran ? 2 : 1;
+                        selectedCategory = null;
                       });
                     },
                     inactiveTrackColor: Colors.green[200],
@@ -54,6 +89,7 @@ class _TransactionPageState extends State<TransactionPage> {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: TextFormField(
+                  controller: jumlahController,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     border: UnderlineInputBorder(),
@@ -69,24 +105,55 @@ class _TransactionPageState extends State<TransactionPage> {
                   style: GoogleFonts.montserrat(fontSize: 16),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: DropdownButton<String>(
-                    value: dropdownValue,
-                    isExpanded: true,
-                    icon: Icon(Icons.arrow_downward),
-                    items: list.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
+              FutureBuilder<List<Category>>(
+                  future: getAllCategory(type),
+                  builder: (context, snapshot) {
+                    // menunggu / loading
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
                       );
-                    }).toList(),
-                    onChanged: (String? value) {}),
-              ),
+                    } else {
+                      if (snapshot.hasData) {
+                        if (snapshot.data!.length > 0) {
+                          selectedCategory = snapshot.data!.first;
+                          // print(snapshot.data);
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: DropdownButton<Category>(
+                                value: selectedCategory == null
+                                    ? snapshot.data!.first
+                                    : selectedCategory,
+                                isExpanded: true,
+                                icon: Icon(Icons.arrow_downward),
+                                items: snapshot.data!.map((Category item) {
+                                  return DropdownMenuItem<Category>(
+                                    value: item,
+                                    child: Text(item.name),
+                                  );
+                                }).toList(),
+                                onChanged: (Category? value) {
+                                  setState(() {
+                                    selectedCategory = value;
+                                  });
+                                }),
+                          );
+                        } else {
+                          return Center(
+                            child: Text('Data kosong'),
+                          );
+                        }
+                      } else {
+                        return Center(
+                          child: Text('Tidak ada data'),
+                        );
+                      }
+                    }
+                  }),
               const SizedBox(height: 25),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: TextField(
+                child: TextFormField(
                   readOnly: true,
                   controller: dateController,
                   decoration: InputDecoration(
@@ -108,13 +175,36 @@ class _TransactionPageState extends State<TransactionPage> {
                   },
                 ),
               ),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  controller: deskripsiController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Deskripsi',
+                  ),
+                ),
+              ),
               const SizedBox(height: 25),
               Center(
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    // print(jumlahController.text);
+                    // print(dateController.text);
+                    // print(deskripsiController.text);
+                    insert(
+                      int.parse(jumlahController.text),
+                      DateTime.parse(dateController.text),
+                      deskripsiController.text,
+                      selectedCategory!.id,
+                    );
+                  },
                   child: Text(
                     'Simpan Data',
-                    style: GoogleFonts.montserrat(fontSize: 20),
+                    style: GoogleFonts.montserrat(
+                        fontSize: 20, fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
